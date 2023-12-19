6b35019306e77d1d013842f683c0e8473c7abd1c
Prepelita-Munchiu Alexandru <dehlordeh@gmail.com>
2023-12-19 21:35:58 +0200

    commit

--- Assets/Scripts/UI/PlayerListSingleUI.cs
+++ Assets/Scripts/UI/PlayerListSingleUI.cs
@@ -2,16 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerListSingleUI : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI playerIdText;
+    [SerializeField] private Image playerThumbnail;
     [SerializeField] private TextMeshProUGUI playerNameText;
     [SerializeField] private TextMeshProUGUI playerShotsText;
 
     public void SetPlayerListSingleData(ulong clientId, string playerName, int playerShots)
     {
-        playerIdText.text = clientId.ToString();
+        PlayerData playerData = GolfGameMultiplayer.Instance.GetPlayerDataFromClientId(clientId);
+        playerThumbnail.color = GolfGameMultiplayer.Instance.GetPlayerColor(playerData.colorId);
         playerNameText.text = playerName.ToUpper();
         playerShotsText.text = playerShots.ToString();
     }
